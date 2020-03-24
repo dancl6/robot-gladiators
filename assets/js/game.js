d4c9423951d56bbbfd13062d6f3ea2085ec83401
Dan Lyons <danielclyons@yahoo.com>
2020-03-24 10:43:20 -0700

    fifth commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,13 +4,12 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyNames) {
     // Alert users that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you  like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -21,20 +20,20 @@ var fight = function() {
     enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
   console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
   );
 // check enemy's health
 if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
+    window.alert(enemyNames + " has died!");
   } else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    window.alert(enemyNames + " still has " + enemyHealth + " health left.");
   }
 
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
   playerHealth = playerHealth - enemyAttack;
   // Log a resulting message to the console so we know that it worked.
   console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
   
 // check player's health
@@ -60,6 +59,8 @@ if (playerHealth <= 0) {
         fight();
       }
   }}
-  
-fight();
+  for(var i = 0; i < enemyNames.length; i++){
+      fight(enemyNames[i]);
+  }
+
   
